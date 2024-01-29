2c9c5feaaaaecc8edf97557a00471dc26489643c
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-01-29 23:25:13 +0600

    edit blade added for unit module

---
+++ resources/views/backend/unit/edit.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.admin_master')
+@section('title','Edit Unit')
+@section('admin_content')
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+
+<!-- Your Blade template -->
+<p class="alert-success" id="success-message">
+    <?php
+    $message = Session::get('message');
+    if($message)
+    {
+        echo $message;
+        Session::put('message', null);
+    }
+    ?>
+</p>
+
+<script>
+    // Add a timer to hide the success message after 3 seconds
+    setTimeout(function () {
+        var successMessage = document.getElementById('success-message');
+        if (successMessage) {
+            successMessage.style.display = 'none';
+        }
+    }, 3000); // 3000 milliseconds = 3 seconds
+</script>
+
+<div class="row-fluid sortable">
+    <div class="box span12">
+        <div class="box-header" data-original-title>
+            <h2><i class="halflings-icon edit"></i><span class="break"></span>Edit Unit</h2>
+
+        </div>
+
+        <div class="box-content">
+            <form class="form-horizontal" action="{{ url('/units/' .$unit->id) }}" method="post" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <fieldset>
+                    <div class="control-group">
+                        <label class="control-label" for="date01">Unit Name</label>
+                        <div class="controls">
+                            <input type="text" class="input-xlarge" name="name" value="{{ $unit->name }}">
+                        </div>
+                    </div>
+
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <button type="reset" class="btn btn-danger">Cancel</button>
+                    </div>
+                </fieldset>
+            </form>
+
+        </div>
+    </div><!--/span-->
+</div><!--/row-->
+</div><!--/row-->
+
+
+@endsection
