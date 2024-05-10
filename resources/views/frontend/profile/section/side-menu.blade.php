328dac581fcb955217fb673514e76081b24f9952
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-11 01:48:24 +0600

    default avatar set for user profile

--- resources/views/frontend/profile/section/side-menu.blade.php
+++ resources/views/frontend/profile/section/side-menu.blade.php
@@ -1,12 +1,16 @@
 <div class="col-lg-4">
         <div class="card mb-4">
-          <div class="card-body text-center">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
-              class="rounded-circle img-fluid" style="width: 150px;">
-            <h5 class="my-3">John Smith</h5>
-            <p class="text-muted mb-1">Full Stack Developer</p>
-            <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
-          </div>
+        <div class="card-body text-center">
+            @if (isset($user->photo))
+            <img src="{{ $user->photo }}" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+            @else
+            <img src="{{ asset('/') }}frontend/images/user-avatar.png" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+            @endif
+            <h5 class="m-t-20 mb-1">{{ $user->name }}</h5>
+            <p class="text-muted mb-1">{{ $user->email }}</p>
+            <!-- <p class="text-muted mb-4">Bay Area, San Francisco, CA</p> -->
+        </div>
+
         </div>
         <div class="card mb-4 mb-lg-0">
           <div class="card-body p-0">
