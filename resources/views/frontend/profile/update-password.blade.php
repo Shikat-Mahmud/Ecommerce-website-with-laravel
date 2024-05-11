9db3f6ffd4989332fa68bc8a1e31beb7f08050ad
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-11 18:48:23 +0600

    profile complete

--- resources/views/frontend/profile/update-password.blade.php
+++ resources/views/frontend/profile/update-password.blade.php
@@ -5,7 +5,7 @@
 <div class="card mb-4" style="border: none;">
     <div class="card-body">
         <div class="row">
-            <div class="p-4">
+            <div class="p-4 w-100">
                 <div class="max-w-xl">
                     @include('frontend.profile.section.update-password')
                 </div>
