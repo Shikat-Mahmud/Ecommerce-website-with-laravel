980a943f917e141e6e507d54e97dbc7d3399caf7
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-11 15:57:00 +0600

    profile menues created

--- resources/views/frontend/profile/section/side-menu.blade.php
+++ resources/views/frontend/profile/section/side-menu.blade.php
@@ -1,3 +1,30 @@
+@push('styles')
+<style>
+  .profile-menu-item{
+    color: #2B2D42;
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  .profile-menu-item:hover{
+    color: #2B2D42;
+  }
+
+  .profile-menu{
+    border-bottom: 1px solid #eee;
+    border-radius: 6px;
+  }
+
+  .profile-menu:hover{
+    background-color: #f2f2f2;
+  }
+
+  .profile-menu-icon{
+    font-size: 15px;
+    margin-right: 5px;
+  }
+</style>
+@endpush
 <div class="col-lg-4">
   <div class="card mb-4" style="border: none;">
     <div class="card-body text-center">
@@ -12,30 +39,18 @@
     </div>
   </div>
     <div class="card mb-4 mb-lg-0" style="border: none;">
-      <div class="card-body p-0">
+      <div class="card-body p-10 my-4">
         <ul class="list-group list-group-flush rounded-3">
-          <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-            <a href="{{ route('profile') }}">Edit Profile</a>
-          </li>
-          <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-            <a href="{{ route('update.password') }}">Update Password</a>
-          </li>
-          <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-            <i class="fab fa-github fa-lg text-body"></i>
-            <p class="mb-0">mdbootstrap</p>
-          </li>
-          <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-            <i class="fab fa-twitter fa-lg" style="color: #55acee;"></i>
-            <p class="mb-0">@mdbootstrap</p>
-          </li>
-          <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-            <i class="fab fa-instagram fa-lg" style="color: #ac2bac;"></i>
-            <p class="mb-0">mdbootstrap</p>
-          </li>
-          <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-            <i class="fab fa-facebook-f fa-lg" style="color: #3b5998;"></i>
-            <p class="mb-0">mdbootstrap</p>
-          </li>
+          <a href="{{ route('profile') }}" class="profile-menu-item">
+            <li class="p-2 pl-3 profile-menu" style="{{ Route::is('profile') ? 'background-color: #717fe0; color: #fff;' : '' }}"><i class="fas fa-user-edit profile-menu-icon"></i> 
+              Edit Profile
+            </li>
+          </a>
+          <a href="{{ route('update.password') }}" class="profile-menu-item">
+            <li class="p-2 pl-3 profile-menu" style="{{ Route::is('update.password') ? 'background-color: #717fe0; color: #fff;' : '' }}"><i class="fas fa-user-edit profile-menu-icon"></i> 
+              Update Password
+            </li>
+          </a>
         </ul>
       </div>
     </div>
