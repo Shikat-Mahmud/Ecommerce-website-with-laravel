72ddd4a355a34f9e7ac4fcb4999af870e17f6b88
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-13 00:07:47 +0600

    faq showing dynamically in the frontend

--- resources/views/frontend/main/faq.blade.php
+++ resources/views/frontend/main/faq.blade.php
@@ -10,8 +10,6 @@
 		</h2>
 	</section>	
 
-
-
 	<!-- Content page -->
 	<section class="bg0 p-t-75 p-b-120">
 		<div class="container">
@@ -22,19 +20,19 @@
 							FAQs
 						</h3>
 
-						<p class="stext-113 cl6 p-b-26">
-							Mauris non lacinia magna. Sed nec lobortis dolor. Vestibulum rhoncus dignissim risus, sed consectetur erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam maximus mauris sit amet odio convallis, in pharetra magna gravida. Praesent sed nunc fermentum mi molestie tempor. Morbi vitae viverra odio. Pellentesque ac velit egestas, luctus arcu non, laoreet mauris. Sed in ipsum tempor, consequat odio in, porttitor ante. Ut mauris ligula, volutpat in sodales in, porta non odio. Pellentesque tempor urna vitae mi vestibulum, nec venenatis nulla lobortis. Proin at gravida ante. Mauris auctor purus at lacus maximus euismod. Pellentesque vulputate massa ut nisl hendrerit, eget elementum libero iaculis.
-						</p>
-
-						<div class="bor16 p-l-29 p-b-9 m-t-22">
-							<p class="stext-114 cl6 p-r-40 p-b-11">
-								Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while.
-							</p>
-
-							<span class="stext-111 cl8">
-								- Steve Job’s 
-							</span>
+						@foreach ($faqs as $faq)
+						<div class="m-b-30">
+							<div class="stext-113 cl6 p-b-10">
+								<h6>{{ $faq->question }}</h6>
+							</div>
+							
+							<div class="bor16 p-l-29">
+								<p class="cl6 p-r-40 p-t-5 p-b-5">
+									{{ strip_tags($faq->answer) }}
+								</p>
+							</div>
 						</div>
+						@endforeach
 					</div>
 				</div>
 
