dbb1c6a1d22ca036761771ff6037ff3067a35f47
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-05-13 00:23:57 +0600

    faq edit complete

---
+++ resources/views/backend/faq/edit.blade.php
@@ -0,0 +1,77 @@
+@extends('admin.admin_master')
+@section('title','Edit FAQ')
+@section('admin_content')
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+
+<!-- Your Blade template -->
+<p class="alert-success" id="success-message">
+    <?php
+    $message = Session::get('message');
+    if($message)
+    {
+        echo $message;
+        Session::put('message', null);
+    }
+    ?>
+</p>
+
+<script>
+    // Add a timer to hide the success message after 3 seconds
+    setTimeout(function () {
+        var successMessage = document.getElementById('success-message');
+        if (successMessage) {
+            successMessage.style.display = 'none';
+        }
+    }, 3000); // 3000 milliseconds = 3 seconds
+</script>
+
+<div class="row-fluid sortable">
+    <div class="box span12">
+        <div class="box-header" data-original-title>
+            <h2><i class="halflings-icon edit"></i><span class="break"></span>Edit FAQ</h2>
+
+        </div>
+
+        <div class="box-content">
+            <form class="form-horizontal" action="{{ url('/faqs/' .$faq->id) }}" method="post" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <fieldset>
+                    <div class="control-group">
+                        <label class="control-label" for="date01">Question</label>
+                        <div class="controls">
+                            <input type="text" class="input-xlarge" name="question" value="{{ $faq->question }}">
+                        </div>
+                    </div>
+
+                    <div class="control-group hidden-phone">
+                        <label class="control-label" for="textarea2">Answer</label>
+                        <div class="controls">
+                            <textarea class="cleditor" name="answer" rows="3" >{{ $faq->answer }}</textarea>
+                        </div>
+
+                    </div>
+
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <button type="reset" class="btn btn-danger">Cancel</button>
+                    </div>
+                </fieldset>
+            </form>
+
+        </div>
+    </div><!--/span-->
+</div><!--/row-->
+
+
+@endsection
